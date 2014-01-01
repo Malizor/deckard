ea7685ea11253dc0ea8d3cfe5f6c48a6464dd13a
Nicolas Delvaux <contact@nicolas-delvaux.org>
2014-01-01 23:53:30 +0100

    Remove an unused setting

--- libdeckard.py
+++ libdeckard.py
@@ -53,7 +53,6 @@ class Session:
         self.gladerunner = gladerunner
         self.content_root = content_root
         self.max_custom_po = 4
-        self.max_file_download = 1500000  # 1.5 MB
         # URL sorted by priority
         # If one URL does not work, the next one will be tried
         self.po_urls = ['http://l10n.gnome.org/media/upload/%s',
